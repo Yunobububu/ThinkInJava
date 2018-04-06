f74350c55194d99adb2b9089294b7dca2d5a78f1
Mikasa <lvjinkebit@163.com>
2018-04-06 22:49:58 +0800

    add Junits

---
+++ src/com/lvjinke/bit/BufferedInputFile.java
@@ -0,0 +1,19 @@
+package com.lvjinke.bit;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedInputFile {
+    public static String read(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        String s;
+        StringBuilder sb = new StringBuilder();
+        while((s = br.readLine())!= null){
+            sb.append(s + "\n");
+        }
+        br.close();
+        return sb.toString();
+    }
+}
