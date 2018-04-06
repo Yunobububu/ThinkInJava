1196e5770871d6e591a862d0bfb525c15cb49795
Mikasa <lvjinkebit@163.com>
2018-04-07 00:53:54 +0800

    FormattedMemoryInput

---
+++ src/com/lvjinke/bit/FormatedMemoryInput.java
@@ -0,0 +1,28 @@
+package com.lvjinke.bit;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.DataInputStream;
+
+public class FormatedMemoryInput {
+    private static String path = null;
+
+    public FormatedMemoryInput(){}
+    public FormatedMemoryInput(String path){
+        this.path = path;
+    }
+
+    public static void read() throws IOException {
+
+        DataInputStream in = new DataInputStream(
+                new ByteArrayInputStream(
+                         BufferedInputFile.read(path
+
+                        ).getBytes()
+                )
+        );
+        while(in.available()!= 0){
+            System.out.println((char)in.readByte());
+        }
+    }
+}
