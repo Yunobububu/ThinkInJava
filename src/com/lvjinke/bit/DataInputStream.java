b8567689ebbe8c21bc183ec07f4a768d0d97fb0b
Mikasa <lvjinkebit@163.com>
2018-04-06 16:45:52 +0800

    DirNameFilter

---
+++ src/com/lvjinke/bit/DataInputStream.java
@@ -0,0 +1,4 @@
+package com.lvjinke.bit;
+
+public class DataInputStream {
+}
--- src/com/lvjinke/bit/DirList.java
+++ src/com/lvjinke/bit/DirList.java
@@ -1,5 +1,42 @@
 package com.lvjinke.bit;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class DirList {
 
+    public DirList() {
+    }
+
+    public  static void main(String[] args){
+        //"." == "/users/xxx/IdeaProjects/ThinkInJava "
+        //即工程的根目录。
+        String path = "./src/com/lvjinke/bit/";
+        String[] list ;
+        File f = new File(path);
+        list = f.list(new DirFilter(".*Stream.*"));
+        Arrays.sort(list,String.CASE_INSENSITIVE_ORDER);
+        for(String s: list){
+            System.out.println(s);
+        }
+
+    }
+
 }
+
+class DirFilter implements FilenameFilter{
+
+    private Pattern pattern;
+    DirFilter(){}
+
+    DirFilter(String regex){
+        pattern = Pattern.compile(regex);
+    }
+    @Override
+    public boolean accept(File dir, String name) {
+        return pattern.matcher(name).matches();
+    }
+}
+
