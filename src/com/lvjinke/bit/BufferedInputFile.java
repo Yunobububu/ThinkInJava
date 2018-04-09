7018e5a7e30d6b003c2a903ad7184972119f0d98
Mikasa <lvjinkebit@163.com>
2018-04-09 15:16:38 +0800

    TextFile

--- src/com/lvjinke/bit/BufferedInputFile.java
+++ src/com/lvjinke/bit/BufferedInputFile.java
@@ -17,3 +17,4 @@ public class BufferedInputFile {
         return sb.toString();
     }
 }
+
---
+++ src/com/lvjinke/bit/TextFile.java
@@ -0,0 +1,44 @@
+package com.lvjinke.bit;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class TextFile extends ArrayList<String>{
+    private static String filename = null;
+    public TextFile(){}
+    public TextFile(String name){
+        this.filename = name;
+    }
+    //Read a file as a list
+    public static String read() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new FileReader(new File(filename).getAbsoluteFile()));
+            String s = null;
+                while((s = in.readLine())!= null){
+                    sb.append(s + "\n");
+                }
+        }catch (Exception e){
+            throw new RuntimeException(e);
+
+        }finally{
+            in.close();
+        }
+        return sb.toString();
+    }
+    //write a file in a method
+    public static void write(String filename,String text){
+        PrintWriter out = null;
+        try {
+
+            out = new PrintWriter(new File(filename).getAbsoluteFile());
+            out.print(text);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }finally {
+            out.close();
+        }
+    }
+
+}
