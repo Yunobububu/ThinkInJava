bb16040646f7e3983df32faffc99db4f1ea8212e
Mikasa <lvjinkebit@163.com>
2018-04-12 15:29:10 +0800

    Stack

---
+++ src/com/lvjinke/bit/Collection/Stack.java
@@ -0,0 +1,21 @@
+package com.lvjinke.bit.Collection;
+//Making a Stack from a LinkedList
+
+import java.util.LinkedList;
+
+public class Stack<T> {
+    private LinkedList<T> linkedList = new LinkedList();
+    public T pop(){
+        return linkedList.removeFirst();
+    };
+    public boolean isEmpty(){
+        return linkedList.isEmpty();
+    };
+    public void push(T e){
+        linkedList.addFirst(e);
+    };
+    public T peek(){
+        return linkedList.getFirst();
+    };
+
+}
