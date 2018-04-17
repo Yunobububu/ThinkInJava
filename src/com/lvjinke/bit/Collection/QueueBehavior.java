3e334c2c84b8c28457d7d3375b450c36a68f7c18
Mikasa <lvjinkebit@163.com>
2018-04-17 21:20:59 +0800

    QueueBehavior

---
+++ src/com/lvjinke/bit/Collection/QueueBehavior.java
@@ -0,0 +1,38 @@
+package com.lvjinke.bit.Collection;
+
+import com.lvjinke.bit.Generics.Generator;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class QueueBehavior {
+    private static int count = 10;
+    static <T> void test(Queue<T> queue, Generator<T> gen){
+        for(int i = 0;i < count;i++){
+            queue.offer(gen.next());
+        }
+        while(queue.peek() != null){
+            System.out.print(queue.poll()+ " ");
+        }
+        System.out.println();
+    }
+    static class Gen implements Generator<String>{
+        String[] s = "one,two,three,four,five,six,seven,eight,night,ten".split(",");
+        int i = 0;
+        @Override
+        public String next() {
+            return s[i++];
+        }
+    }
+    public static void execute(){
+        test(new LinkedList<String>(),new Gen());
+        test(new PriorityQueue<String>(),new Gen());
+        test(new ArrayBlockingQueue<String>(count),new Gen());
+        test(new ConcurrentLinkedDeque<String>(),new Gen());
+        test(new PriorityBlockingQueue<String>(),new Gen());
+    }
+}
