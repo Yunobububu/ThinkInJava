f0de781ef6b54af1cea11ab51c8950c0d2283401
Mikasa <lvjinkebit@163.com>
2018-04-18 08:38:52 +0800

    Callable

---
+++ src/com/lvjinke/bit/Concurrency/SingleThreadExecutor.java
@@ -0,0 +1,14 @@
+package com.lvjinke.bit.Concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SingleThreadExecutor {
+    public static void execute(){
+        ExecutorService exec = Executors.newSingleThreadExecutor();
+        for(int i = 0;i < 5;i++){
+            exec.execute(new LiftOff());
+        }
+        exec.shutdown();
+    }
+}
